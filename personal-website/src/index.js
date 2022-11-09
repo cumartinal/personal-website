3d2e09e22a85c87f51e08332a371757d21d13c3f
Martin Cuesta Allende <cumartinal@gmail.com>
2022-11-09 22:37:04 +0000

    Remove unnecessary files, update favicon, making very basic styling

--- personal-website/src/index.js
+++ personal-website/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(el);
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ color: "#128743" }}>Hi, I'm Martin!</h1>
+      <h2>
+        I'm a 4th Year MEng Computer Science (Human Computer Interaction)
+        student at the University of Manchester
+      </h2>
+    </div>
+  );
+}
+
+root.render(<App />);
